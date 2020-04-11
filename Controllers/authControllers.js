958b77ebe2621d1b75f156e0fb0000187e1e1b68
myles-richard <myrich22@gmail.com>
2020-04-11 16:16:56 -0700

    log in user works

--- Controllers/authControllers.js
+++ Controllers/authControllers.js
@@ -1,6 +1,7 @@
 const db = require('../models');
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
 
 
 const signUp = (req, res) => {
@@ -22,8 +23,47 @@ const getUser = (req, res) => {
     res.send('get logged in user')
 }
 
-const logIn = (req, res) => {
-    res.send('login user')
+const logIn = async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body;
+    
+    try {
+        //Find if user exist check email 
+        let user = await db.User.findOne({ email });
+        
+        if(!user) {
+            return res.status(400).json({ msg: 'Invalid Credentials' });
+        }
+
+        //if there is user, continue to check password 
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        //return true or false if match
+        if(!isMatch) {
+            return res.status(400).json({ msg: 'Invalid Credentials '});
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload,process.env.JWT_SECRET, {
+            expiresIn: 360000
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({ token })
+        });
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
 }
 
 module.exports = {
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../Controllers");
+const { check } = require('express-validator');
 
 // Auth
 router.post("/register", ctrl.auth.signUp);
-router.post("/", ctrl.auth.logIn);
+// authenticating user 
+router.post("/",[
+    check('email', 'Please include valid email').isEmail(),
+    check('password', 'Password is required').exists()
+], ctrl.auth.logIn);
 //get logged in user 
 router.get('/', ctrl.auth.getUser);
 // router.get("/verify", ctrl.auth.verify);
